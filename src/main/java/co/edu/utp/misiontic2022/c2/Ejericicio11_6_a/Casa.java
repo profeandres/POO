374cd23efc1c0c064594608fa5645286aa42720f
Andres Cantillo <andrescantillo@hotmail.com>
2021-07-20 12:19:55 -0500

    Eliminación de package Casa y terminado el ejercicio 11.6.a

---
+++ src/main/java/co/edu/utp/misiontic2022/c2/Ejericicio11_6_a/Casa.java
@@ -0,0 +1,17 @@
+package co.edu.utp.misiontic2022.c2.Ejericicio11_6_a;
+/*
+Construir una clase con tres atributos de una casa, crear la Clase Casa
+y crear su método Constructor que le asigne valores a los atributos que
+se crearon. Adicional construya un programa Java que utilice esta
+Clase Casa y muestre sus resultados
+*/
+public class Casa {
+    int Valor;
+    int Calle;
+    int Cra;
+    public Casa() {
+        Valor=20000000;
+        Calle=24;
+        Cra=37;        
+    }
+}
---
+++ src/main/java/co/edu/utp/misiontic2022/c2/Ejericicio11_6_a/MainCasa.java
@@ -0,0 +1,10 @@
+package co.edu.utp.misiontic2022.c2.Ejericicio11_6_a;
+
+public class MainCasa {
+    public static void main(String[] args) {
+        Casa Casa1=new Casa();
+
+        System.out.println("La casa vale: "+Casa1.Valor);
+        System.out.println("y queda ubicada en la calle "+Casa1.Calle+" con cra "+Casa1.Cra);
+    }
+}
