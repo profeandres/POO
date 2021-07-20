887b5b313f107132c2afd04436b8de4c9aed2e6d
Andres Cantillo <andrescantillo@hotmail.com>
2021-07-20 12:00:30 -0500

    Ejemplo 2 de poo, voy entendiendo mejor como asignar atributos

---
+++ src/main/java/co/edu/utp/misiontic2022/c2/Figuras/ClaseTriangulos.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic2022.c2.Figuras;
+
+public class ClaseTriangulos {
+    int altura;
+    int base;
+    int area;
+
+    public ClaseTriangulos(){
+        base=5;
+        altura=10;
+        area= (base*altura)/10;
+    }
+}
---
+++ src/main/java/co/edu/utp/misiontic2022/c2/Figuras/Main.java
@@ -0,0 +1,11 @@
+package co.edu.utp.misiontic2022.c2.Figuras;
+
+public class Main {
+    public static void main(String[] args) {
+        ClaseTriangulos Trian1= new ClaseTriangulos();
+
+        System.out.println("La altura del triángulo es: "+Trian1.altura);
+        System.out.println("La base del triángulo es: "+Trian1.base);
+        
+    }
+}
