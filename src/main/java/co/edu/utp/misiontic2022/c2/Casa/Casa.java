31b3786150c04049f97727c81d46424f243cf864
Andres Cantillo <andrescantillo@hotmail.com>
2021-07-20 12:02:10 -0500

    Quite un comentario

--- src/main/java/co/edu/utp/misiontic2022/c2/Casa/Casa.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/Casa/Casa.java
@@ -1,11 +1,5 @@
 package co.edu.utp.misiontic2022.c2.Casa;
 
-/*
-Construir una clase con tres atributos de una casa, crear la Clase Casa
-y crear su método Constructor que le asigne valores a los atributos que
-se crearon. Adicional construya un programa Java que utilice esta
-Clase Casa y muestre sus resultados
-*/
 public class Casa
 {
     String Color;
